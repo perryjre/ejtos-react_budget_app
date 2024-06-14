e05dfb1c6bfdecfd07119e33ce31a95f90ed7acb
perryjre <perryjre@github.com>
2024-06-14 18:20:27 -0400

    finishes lab

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,22 +4,40 @@ import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
     const { dispatch, currency } = useContext(AppContext);
-    var name;
+    var dolsel = false;
+    var pndsel = false;
+    var eursel = false;
+    var rupsel = false;
     switch (currency){
         case '$':
-            name = 'Dollar';
+            dolsel = "selected";
+            pndsel = false;
+            eursel = false;
+            rupsel = false;
             break;
         case '£':
-            name = 'Pound';
+            dolsel = false;
+            pndsel = "selected";
+            eursel = false;
+            rupsel = false;
             break;
         case '€':
-            name = 'Euro';
+            dolsel = false;
+            pndsel = false;
+            eursel = "selected";
+            rupsel = false;
             break;
         case '₹':
-            name = 'Rupee';
+            dolsel = false;
+            pndsel = false;
+            eursel = false;
+            rupsel = "selected";
             break;
         default:
-            name = '';
+            dolsel = false;
+            pndsel = false;
+            eursel = false;
+            rupsel = false;
     }
     const changeCurrency = (val) => {
         dispatch({
@@ -28,15 +46,16 @@ const Currency = () => {
         })
     }
     return (
-        <label for="selectCurrency">Currency
-        <select className="custom-select" name="selectCurrency" id="selectCurrency" onChange={(event) => changeCurrency(event.target.value)}>
-            <option defaultValue>Currency {currency}{name}</option>
-            <option value="$" name="dollar">$ Dollar</option>
-            <option value="£" name="pound">£ Pound</option>
-            <option value="€" name="euro">€ Euro</option>
-            <option value="₹" name="rupee">₹ Rupee</option>
-        </select>
-        </label>
+        <div className="currencyselect">
+            <label for="selectCurrency">Currency
+            <select name="selectCurrency" id="selectCurrency" onChange={(event) => changeCurrency(event.target.value)}>
+                <option value="$" selected={dolsel} name="dollar">$ Dollar</option>
+                <option value="£" selected={pndsel} name="pound">£ Pound</option>
+                <option value="€" selected={eursel} name="euro">€ Euro</option>
+                <option value="₹" selected={rupsel} name="rupee">₹ Rupee</option>
+            </select>
+            </label>
+        </div>
     );
 };
 export default Currency;
